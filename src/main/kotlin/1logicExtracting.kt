778b18317d52c216c63b71a26043724e4bd37339
wout.werkman <wout.werkman@jetbrains.com>
2024-06-10 08:39:10 +0200

    Logic extracting example

---
+++ src/main/kotlin/1logicExtracting.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("all plus 1")
+    printAllPlusOne(listOf(1, 2, 3))
+    println("all plus 2")
+    printAllPlusTwo(listOf(1, 2, 3))
+}
+
+private fun printAllPlusOne(numbers: Iterable<Int>) {
+    for (number in numbers) {
+        println(number + 1)
+    }
+}
+
+private fun printAllPlusTwo(numbers: Iterable<Int>) {
+    for (number in numbers) {
+        println(number + 1)
+    }
+}
+
